850973546e4be3524862e766c0d7cbd04441be73
Phil-Swift-Furry <122639044+Phil-Swift-Furry@users.noreply.github.com>
2023-01-13 14:37:14 -0800

    cleanup

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 load_dotenv()
 
 # get the token and instantiate the bot
-DISCORD_TOKEN = ""
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix='m!',intents=discord.Intents.all())
 
 # check for event (ready)
@@ -20,14 +20,14 @@ async def on_ready():
     print('Connected to bot: {}'.format(bot.user.name))
     print('Bot ID: {}'.format(bot.user.id))
 
-#grab chID
+#grab chID for library
 @bot.command()
 async def ch(ctx):
     chID = ctx.channel.id
     await WTTF(chID, ctx)
     
 
-#append chID
+#append chID into library
 async def WTTF(chID, ctx):
     with open('chnl.txt', 'r+') as f:
         match = False
@@ -39,13 +39,9 @@ async def WTTF(chID, ctx):
             await ctx.send(f'Succesfully added <#{chID}>.')
         else: 
             await ctx.send(f'<#{chID}> is already in our database!')
-# sets channel and format (yes this is custom to a few servers)
-#ID1=869743854964846634
-#ID2=979421338387222648
-#ID3=898141938534981673
-date_format = "%m/%d/%Y"
 
 # on cron time ready
+date_format = "%m/%d/%Y"
 @aiocron.crontab('0 17 * * fri') # Every friday at 5pm UTC
 async def cornjob1():
     # calculate difference in dates
@@ -54,39 +50,20 @@ async def cornjob1():
     diff = today - morbius
 
     # set the channel for the bot's context
-    #ch1 = bot.get_channel(ID1)
-    #ch2 = bot.get_channel(ID2)
-    #ch3 = bot.get_channel(ID3)
-    chs = [ch1, ch2, ch3]
     with open('chnl.txt', 'r') as f:
-        while True:
-            line = f.readline()
-            if not line == "":
-                i+=1
-                chs.append(line)
-            else:
-                break
-
+      chs = f.readlines()
     for x in chs:
         # async function completion: send string in bot's contexted channel
-        await x.send('Its morbin time, it has been ' + str(diff.days) + ' days since Morbius released.')
+        await bot.get_channel(int(x)).send('Its morbin time, it has been ' + str(diff.days) + ' days since Morbius released.')
 
-#test command
+# test command
 @bot.command()
 async def test(ctx):
     today = datetime.date.today()
     morbius = datetime.date(2022, 3, 31)
     diff = today - morbius
-    chs = []
-    i=0
     with open('chnl.txt', 'r') as f:
-        while True:
-            line = f.readline()
-            if not line == "":
-                i+=1
-                chs.append(line)
-            else:
-                break
+        chs = f.readlines()
     for x in chs:
         await bot.get_channel(int(x)).send('Its morbin time, it has been ' + str(diff.days) + ' days since Morbius released.')
 
